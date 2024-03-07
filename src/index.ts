bb934314be6eb301367b6c316045914b10793b63
Xeu <thankrain@qq.com>
2024-03-07 12:08:45 +0000

    feat. 支持仅当匹配到关键字后再进行邮件提取

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import { Bot, Context, Schema } from 'koishi'
-import { format } from 'path'
+import { Context, Schema } from 'koishi'
 
 export const name = 'mail-extractor'
 export const inject = []
 
 export interface Pattern {
+  keyword: string,
   regex: string,
   modifier: string,
   format: string,
 }
 interface PatternRegExp {
+  keywords: string[],
   regex: RegExp,
   format: string
 }
@@ -27,7 +28,8 @@ export const Config: Schema<Config> = Schema.object({
   mails: Schema.array(Schema.string().description("邮件地址，如：`example@example.com`").required()).description('需要监听的邮件地址列表:`adapter-mail`的`selfId`字段').required(),
   patterns: Schema.array(
     Schema.object({
-      regex: Schema.string().default(`\\b\\d{6,}\\b`).description("匹配正则表达式"),
+      keyword: Schema.string().default(`验证码|校验码|检验码|确认码|激活码|动态码|安全码|验证代码|校验代码|检验代码|激活代码|确认代码|动态代码|安全代码|登入码|认证码|识别码|短信口令|动态密码|交易码|上网密码|随机码|动态口令|驗證碼|校驗碼|檢驗碼|確認碼|激活碼|動態碼|驗證代碼|校驗代碼|檢驗代碼|確認代碼|激活代碼|動態代碼|登入碼|認證碼|識別碼|Code|code|CODE`).description("仅当邮件中包含以下关键字时才启用下面的正则表达式匹配，多个关键字用`|`分割"),
+      regex: Schema.string().default(`\\b\\d{6,8}\\b`).description("匹配正则表达式"),
       modifier: Schema.string().default(`igm`).description("[正则表达式修饰符](https://www.runoob.com/js/js-regexp.html)"),
       format: Schema.string().default("$0").description("输出的格式化样式，[使用$0-$9表示匹配到的组的内容](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/RegExp/n)\\\n 使用`{content}`表示邮件纯文本内容"),
     })
@@ -45,6 +47,7 @@ export function apply(ctx: Context, config: Config) {
   logger.info(`可用的邮件地址列表：${ctx.platform("mail").bots.map(bot => `${bot.selfId}`).join(", ")}`)
   let mails = config.mails;
   let patterns: PatternRegExp[] = config.patterns.map(p => ({
+    keywords: p.keyword.split("|"),
     regex: new RegExp(p.regex, p.modifier),
     format: p.format
   }));
@@ -57,6 +60,14 @@ export function apply(ctx: Context, config: Config) {
       logger.info(`${session.platform},${session.channelId} => ${session.content}`)
       let content = session.content;
       for (let pattern of patterns) {
+
+        // 匹配关键字
+        if (pattern.keywords.length > 0) {
+          if (pattern.keywords.find(keyword => content.includes(keyword)) == null) {
+            continue
+          }
+        }
+
         let result = pattern.regex.exec(content);
         logger.info(`匹配结果：${result}`)
         if (result != null) {
@@ -86,6 +97,14 @@ export function apply(ctx: Context, config: Config) {
         logger.info(`${session.platform},${session.channelId} => ${session.content}`)
         let content = session.content;
         for (let pattern of patterns) {
+
+          // 匹配关键字
+          if (pattern.keywords.length > 0) {
+            if (pattern.keywords.find(keyword => content.includes(keyword)) == null) {
+              continue
+            }
+          }
+
           let result = pattern.regex.exec(content);
           if (result != null) {
             let output = pattern.format;
@@ -94,13 +113,14 @@ export function apply(ctx: Context, config: Config) {
               logger.info(`replace: $${i} => ${str}`)
             })
             output = output.replaceAll("{content}", content)
-            let debugOutput = `${output}\n\n匹配结果：\n${result.map((str,i)=>`$${i} => ${str}`).join("\n")}`
+            let debugOutput = `${output}\n\n匹配结果：\n${result.map((str, i) => `$${i} => ${str}`).join("\n")}`
             session.bot.createMessage(session.channelId, debugOutput)
             if (!if_continue) {
               return
             }
           }
         }
+        session.bot.createMessage(session.channelId, `什么都没匹配到`)
       }
     })
   }
